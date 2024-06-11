deffdd8cc25e12efa466c85b33808bdf13872df8
luisaxsz <marialuisalameirao@gmail.com>
2024-06-11 10:54:01 -0300

    test: testes de delete e findById

---
+++ src/main/java/com/example/ApiFilmes/exception/EntityNotFoundExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.ApiFilmes.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class EntityNotFoundExceptionHandler{
+
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Filme não encontrado");
+  }
+}
